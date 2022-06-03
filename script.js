2b9733d622220c9e7a78181f2596f950141d1e6b
Rika <mkmtrkk26@gmail.com>
2022-06-03 22:44:54 +0900

    buttonタグ進めてみました

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+    //
+    $('.add_btn').on('click',function(){
+        $('.add').toggleClass('hide');
+    })
+    $('.num_btn').on('click',function(){
+        $('.num').toggleClass('hide');
+    })
+    $('.email_btn').on('click',function(){
+        $('.email').toggleClass('hide');
+    })
+
+
+
+
+    
+})
